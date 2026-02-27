2b970b0b16d21c48d6d5cf2810f3963796003b4b
Esteban De La Fuente Rubio <esteban@delaf.cl>
2026-02-27 17:04:46 -0300

    Se cambia a PHP 8.5

--- php-cs-fixer.php
+++ php-cs-fixer.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * Derafu: Foundation - Base for Derafu's Projects.
+ * Derafu: Twig - UI Component and Extension Library.
  *
  * Copyright (c) 2025 Esteban De La Fuente Rubio / Derafu <https://www.derafu.dev>
  * Licensed under the MIT License.
